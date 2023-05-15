4ce37a867ebfb6bf74913dbd28e91ebd211b3bd1
Langershivam <shivamlanger2001@gmail.com>
2023-05-15 12:51:37 +0530

    weather app

--- weather.js
+++ weather.js
@@ -1,9 +1,15 @@
+window.onload=getWeather();
 function getWeather() {
-   const city = document.getElementById("search").value;
-   const apiKey = "838d170d19241e7aa864762caaa8ecdf";
- 
+   let city = "";
+   if(document.getElementById("search").value.length!=0){
+    city=document.getElementById("search").value;
+   }
+   else{
+    city="jammu";
+   }
+   
    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=838d170d19241e7aa864762caaa8ecdf&units=metric`;
- 
+   
    let City = document.getElementById("city");
    let humidity = document.getElementById("humidity");
    let temperature = document.getElementById("temperature");
@@ -13,14 +19,22 @@ function getWeather() {
      const weather = fetch(url);
      resolve(weather);
    });
- 
+   
    promise
      .then((response) => response.json())
      .then((data) => {
+      if(data.name==undefined){
+        City.textContent = data.message;
+        temperature.textContent = "";
+        humidity.textContent = "";
+        wind.textContent = "";
+      }
+      else{
        City.textContent = `${data.name}`;
-       temperature.textContent = `${data.main.temp}`;
-       humidity.textContent = `${data.main.humidity}`;
-       wind.textContent = `${data.wind.speed}`;
+       temperature.textContent = `${data.main.temp}°C`;
+       humidity.textContent = `${data.main.humidity}%`;
+       wind.textContent = `${data.wind.speed} m/s`;
+      }
      });
  
    document.getElementById("weather").style.display = "flex";
